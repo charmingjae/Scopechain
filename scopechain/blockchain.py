bef4f7fb036f0d96e3eff2495cc1238d2de8b7e6
chaminjae <beingeskimo@gmail.com>
2021-05-19 23:15:52 +0900

    UPDATE : use two boundary thread

--- scopechain/blockchain.py
+++ scopechain/blockchain.py
@@ -191,12 +191,7 @@ class Blockchain:
         # return proof
 
         proof1 = 0
-        proof2 = 50001
-        proof3 = 100001
-        proof4 = 150001
-        proof5 = 200001
-        proof6 = 250001
-        proof7 = 300001
+        proof2 = 150001
 
         def fun1(last_proof, proof, last_hash, start, finish, idx):
             global proof_Result
@@ -204,6 +199,8 @@ class Blockchain:
             global arr
             # print('fun{0} now flags : {1}'.format(idx, flags))
             for i in range(start, finish):
+                if i == (finish-1):
+                    print('equals')
                 if flags:
                     break
                 if self.valid_proof(last_proof, proof, last_hash) is True:
@@ -216,32 +213,15 @@ class Blockchain:
                 proof += 1
 
         th1 = threading.Thread(target=fun1, args=(
-            last_proof, proof1, last_hash, 1, 50001, 1))
+            last_proof, proof1, last_hash, 0, 150001, 1))
         th2 = threading.Thread(target=fun1, args=(
-            last_proof, proof2, last_hash, 50001, 100001, 2))
-        th3 = threading.Thread(target=fun1, args=(
-            last_proof, proof3, last_hash, 100001, 150001, 3))
-        th4 = threading.Thread(target=fun1, args=(
-            last_proof, proof4, last_hash, 150001, 200001, 4))
-        th5 = threading.Thread(target=fun1, args=(
-            last_proof, proof5, last_hash, 200001, 250001, 5))
-        th6 = threading.Thread(target=fun1, args=(
-            last_proof, proof6, last_hash, 250001, 300001, 6))
-        th7 = threading.Thread(target=fun1, args=(
-            last_proof, proof7, last_hash, 300001, 350001, 7))
+            last_proof, proof2, last_hash, 150001, 300001, 2))
 
         th1.start()
         th2.start()
-        th3.start()
-        th4.start()
-        th5.start()
-        th6.start()
 
-        # th6.join()
-
-        th7.start()
-
-        # th7.join()
+        th1.join()
+        th2.join()
 
         # if not th2.is_alive():
         #     print('th2 is not alive')
@@ -260,6 +240,8 @@ class Blockchain:
         print('현재까지 걸린 시간 : ', time_cnt)
         print('평균시간 : ', time_cnt/100)
         print('범위 별 정보 : ', arr)
+        print('None : ', 100 - sum(arr))
+
         return proof_Result
 
     @ staticmethod
