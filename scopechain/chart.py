b1360bd806146f22efaa968d2bf43897c2a7ebda
chaminjae <beingeskimo@gmail.com>
2021-05-21 15:27:08 +0900

    UPDATE : Chart

--- scopechain/chart.py
+++ scopechain/chart.py
@@ -1,27 +1,31 @@
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
+
+# xlabel
 cycle = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # use 1 thread
 simul1 = [0.58, 0.54, 0.69, 0.58, 0.56, 0.51, 0.71, 0.69, 0.75, 0.50]
 
 # use 2 threads
-simul2 = [0.47, 0.51, 0.64, 0.56, 0.41, 0.48, 0.45, 0.53, 0.56, 0.58]
+simul2 = [0.49, 0.40, 0.40, 0.44, 0.37, 0.48, 0.50, 0.51, 0.45, 0.41]
 
-# use 4 threads
-simul3 = [0.39, 0.41, 0.39, 0.45, 0.38, 0.38, 0.37, 0.46, 0.45, 0.34]
+# use 5 threads
+simul3 = [0.34, 0.39, 0.32, 0.33, 0.39, 0.33, 0.35, 0.36, 0.37, 0.37]
 
-# use 7 threads with no join
-simul4 = [0.29, 0.33, 0.31, 0.31, 0.31, 0.32, 0.29, 0.31, 0.34, 0.25]
+# use 7 threads
+simul4 = [0.34, 0.31, 0.30, 0.34, 0.34, 0.34, 0.30, 0.34, 0.34, 0.34]
 
-# use 7 threads with join
 
-simul5 = [0.34, 0.38, 0.36, 0.38, 0.33, 0.33, 0.37, 0.36, 0.35, 0.35]
+fig = plt.figure()
 
-plt.plot(cycle, simul1)
-plt.plot(cycle, simul2)
-plt.plot(cycle, simul3)
-plt.plot(cycle, simul4)
-plt.plot(cycle, simul5)
+ax1 = fig.add_subplot(111)
+
+ax1.plot(cycle, simul1, label='1 thread')
+ax1.plot(cycle, simul2, label='2 thread')
+ax1.plot(cycle, simul3, label='5 thread')
+ax1.plot(cycle, simul4, label='7 thread')
+
+ax1.legend()
 
 plt.show()
