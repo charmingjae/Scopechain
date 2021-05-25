a8499a86bfefefd373bf728404b4dd8c75b9e009
chaminjae <beingeskimo@gmail.com>
2021-05-25 15:43:09 +0900

    UPDATE : multiprocessing

--- scopechain/test/mp.py
+++ scopechain/test/mp.py
@@ -1,79 +1,18 @@
-from multiprocessing import Process, Value
-from ctypes import c_bool
 import multiprocessing
 import time
 
-
-# if flags.value is True:
-#     print('True!!!!')
-# else:
-#     print('False!!!')
+start_time = time.time()
 
 
-# def fun1(flags):
-#     for i in range(1, 11):
-#         print(i)
-#         if i == 5:
-#             flags.value = True
-#             print('NOW FLAG : ', flags.value)
-#             break
-#         print('NOW FLAG : ', flags.value)
-#         time.sleep(1)
+def count(name):
+    for i in range(1, 50001):
+        print(name, " : ", i)
 
 
-# def fun2(flags):
-#     for i in range(100, 111):
-#         if flags.value is True:
-#             break
-#         print(i)
-#         print('NOW FLAG : ', flags.value)
-#         time.sleep(1)
-
-def fun1(flags):
-    for i in range(1, 11):
-        print(i)
-        if i == 5:
-            flags.value = True
-            print('NOW FLAG : ', flags.value)
-            break
-        print('NOW FLAG : ', flags.value)
-        time.sleep(1)
-
-
-def fun2(flags):
-    for i in range(100, 111):
-        if flags.value is True:
-            break
-        print(i)
-        print('NOW FLAG : ', flags.value)
-        time.sleep(1)
-
-
-def test():
-    flags = Value(c_bool, False)
-
-    p1 = Process(target=fun1, args=(flags,))
-    p2 = Process(target=fun2, args=(flags,))
-
-    p1.start()
-    p2.start()
-
-    p1.join()
-    p2.join()
-
-    print('flags : ', flags.value)
+num_list = ['p1', 'p2', 'p3', 'p4']
 
 
 if __name__ == '__main__':
-    test()
-
-    # flags = Value(c_bool, False)
-    # print(flags.value)
-    # p1 = Process(target=fun1, args=(flags,))
-    # p2 = Process(target=fun2, args=(flags,))
-
-    # p1.start()
-    # p2.start()
-
-    # p1.join()
-    # p2.join()
+    pool = multiprocessing.Pool(processes=2)
+    pool.map(count, num_list)
+    pool.close()
